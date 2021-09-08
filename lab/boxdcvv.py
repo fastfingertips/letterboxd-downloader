dbb1e51b6f870f93b2afcde0937a01d27b91633b
Aade <bugra.coskun@outlook.com>
2021-09-08 13:47:02 +0300

    Border expansion operations
    + Changes were made to bundle the movies together.
    + Now we can shoot all the movies on the list, not 100.

--- lab/boxdcvv.py
+++ lab/boxdcvv.py
@@ -13,6 +13,36 @@ def readpage(r_url):
     return soup
 
 
+def countrooms(r_article):
+    data_count = 0
+    for rooms in r_article:
+        data_count += 1
+    return data_count
+
+
+def pullfilms(r_count, r_soup):
+    # > Çekilen sayfa kodları, bir filtre uygulanarak daraltıldı.
+    articles = r_soup.find('ul', attrs={
+        'class': 'poster-list -p70 film-list clear film-details-list'}).find_all("li")
+    dongu_no = r_count
+    for rooms in articles:
+        # Oda ismini çektik
+        film = rooms.find(
+            'h2', attrs={'class': 'headline-2 prettify'})
+        film_adi = film.find('a').text
+        # Film yılı bazen boş olabiliyor. Önlem alıyoruz"
+        try:
+            film_yili = film.find('small').text
+        except:
+            film_yili = "Yok"
+        # Her seferinde Csv dosyasına çektiğimiz bilgileri yazıyoruz.
+        print(f'{dongu_no}) {film_adi} ({film_yili})')
+        writer.writerow(
+            [str(dongu_no), str(film_adi), str(film_yili)])
+        dongu_no += 1
+    return dongu_no
+
+
 if True:
     # > Kullanıcı eğer domainden değilde direkt olarak girerse yazıyı küçültüyoruz.
     user_name = str(input("Username(Not display name): ")).lower()
@@ -26,17 +56,16 @@ if True:
     main_protocol = "https://"
     site_url = "letterboxd.com"
     domain = main_protocol + site_url
-    url = f'{domain}/{user_name}/list/{list_name}/detail/'
+    mini_url = f'{domain}/{user_name}/list/{list_name}'
+    url = f'{mini_url}/detail/page/'
     # > Domain'in doğru olup olmadığı kullanıcıya sorulur,
     # > doğruysa kullanıcı enter'a basar ve program verileri çeker.
-    ent = input("Is the domain correct? (Enter): "+url + "\n: ").lower()
+    ent = input(
+        f"Is the domain correct? (Enter): {mini_url}\n: ").lower()
+
     if ent == "":
         # > Parçalanan Domain toplanarak, istek atıldı.
-
-        soup = readpage(url)
-        # > Çekilen sayfa kodları, bir filtre uygulanarak daraltıldı.
-        articles = soup.find('ul', attrs={
-            'class': 'poster-list -p70 film-list clear film-details-list'}).find_all("li")
+        soup = readpage(mini_url)
 
         # > Not: Sayfa sayısını bulmak için li'leri sayma. Son sayıyı al.
         try:
@@ -44,20 +73,17 @@ if True:
             lastPage_No = soup.find(
                 'div', attrs={'class': 'paginate-pages'}).find_all("li")[-1].a.text
             print(f"{lastPage_No} sayfası olan bir listedir.")
-            last_page_url = f"page/{lastPage_No}"
+            pageNo = lastPage_No
 
             # > Son sayfaya bağlanmak için bir ön hazırlık.
-            lastsoup = readpage(url + last_page_url)
+            lastsoup = readpage(url + lastPage_No)
             # > Sayfa kodları çekildi.
             lastarticles = lastsoup.find('ul', attrs={
                 'class': 'poster-list -p70 film-list clear film-details-list'}).find_all("li")
-            lastpage_fcount = 0
-            for lastrooms in lastarticles:
-                lastpage_fcount += 1
+            lastpage_fcount = countrooms(lastarticles)
             # < Film sayısı öğrenildi.
             # > Toplam film sayısını belirlemek.
             film_sayisi = ((int(lastPage_No)-1)*100)+lastpage_fcount
-            print((int(lastPage_No)-1)*100+lastpage_fcount)
             print(f"Bu listedeki film sayısı: {film_sayisi}")
         except:
             # > Listede 100 ve 100'den az film sayısı olduğunda sayfa sayısı için bir link oluşturulmaz.
@@ -69,20 +95,10 @@ if True:
             writer.writerow(["Sıra", "Filmİsmi", "YayınYılı"])
             # Filmleri çekiyoruz
             dongu_no = 1
-            for rooms in articles:
-                # Oda ismini çektik
-                film = rooms.find(
-                    'h2', attrs={'class': 'headline-2 prettify'})
-                film_adi = film.find('a').text
-                # Film yılı bazen boş olabiliyor. Önlem alıyoruz"
-                try:
-                    film_yili = film.find('small').text
-                except:
-                    film_yili = "Yok"
-                # Her seferinde Csv dosyasına çektiğimiz bilgileri yazıyoruz.
-                print(f'{dongu_no}) {film_adi} ({film_yili})')
-                writer.writerow(
-                    [str(dongu_no), str(film_adi), str(film_yili)])
-                dongu_no += 1
+            for x in range(int(lastPage_No)):
+                print(f"Connecting to: {url}{str(x+1)}")
+                current_soup = readpage(url + str(x+1))
+                dongu_no = pullfilms(dongu_no, current_soup)
+            # Dosyayı kapattık
             file.close()
 print(f'{dongu_no-1} Film bulundu. Tüm filmler {csv_name +" dosyasına"} aktarıldı.')
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+cls
+python lab/boxdcvv.py
+pause
