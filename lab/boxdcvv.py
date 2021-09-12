71ef7afc391637f8a04da5f9922ccde4d00529dd
Aade <bugra.coskun@outlook.com>
2021-09-12 21:01:28 +0300

    fixes and new features
    + Filter types are displayed.
    + The bug in choosing the year has been fixed.
    + Listing on the terminal screen has been improved.
    + The publication date and editing date of the list were calculated with a new library and exhibited.

--- lab/boxdcvv.py
+++ lab/boxdcvv.py
@@ -1,4 +1,6 @@
-from datetime import datetime
+import arrow
+from datetime import datetime, timedelta, date, timezone
+from dateutil.relativedelta import relativedelta
 from bs4 import BeautifulSoup
 from typing import Collection
 from pandas import DataFrame
@@ -7,8 +9,13 @@ import csv
 import sys
 import os
 # Hem temiz bir başlangıç hem de yeniden başlatmalarda Press any key.. mesajını kaldırmak için.
-zaman = datetime.now().strftime('%d%m%Y%H%M')
 os.system('cls')
+run_time = datetime.now().strftime('%d%m%Y%H%M%S')
+print(f'Session hash: #{run_time}')
+
+
+def test_pause():
+    os.system('echo Test için durduruluyor. & pause >nul')
 
 
 def filtre_sor():
@@ -21,14 +28,16 @@ def filtre_sor():
             filter_empty_items = 0
             while True:
                 decadeyear_dory = input(
-                    f"> Want Decade or Year filter? [D/Y/N]: ").lower()
+                    f"\n> Want Decade or Year filter? [D/Y/N]: ").lower()
                 if decadeyear_dory == "d":
                     # Çekmek yerine ürettik.
-                    decade_year = 1870
+                    decade_year = 1870  # min decade year
+                    max_multiply = 16  # 16*10 + 1870
                     decade_years = []
-                    decade_num = 0
                     decade_nums = []
-                    for i in range(16):
+                    decade_num = 0
+                    for i in range(max_multiply):
+                        # blank = ' ' if i in range(10) else ''
                         blank = ' ' if i < 10 else ''
                         print(f'[{decade_num}] {blank}{decade_year}s')
                         decade_years.append(int(decade_year))
@@ -38,20 +47,21 @@ def filtre_sor():
                     i_decadeyear = int(input(f"> Decade [Num]: "))
                     print(
                         f'Selected: [{i_decadeyear}]: {decade_years[i_decadeyear]}s\n')
-                    msg_decadeyear = f'Decade:  [{i_decadeyear}]: {decade_years[i_decadeyear]}s\n'
+                    msg_decadeyear = f'  \u25E6 Decade:  [{i_decadeyear}]: {decade_years[i_decadeyear]}s\n'
                     w_decadeyear = f"/decade/{decade_years[i_decadeyear]}s"
                     decadeyear_confirm = True
                 elif decadeyear_dory == "y":
                     while True:
                         i_decadeyear = int(input(f"> Year [1870-2029]: "))
-                        msg_decadeyear = f'Year: {i_decadeyear}\n'
+                        msg_decadeyear = f'   Year: {i_decadeyear}\n'
                         w_decadeyear = f"/year/{i_decadeyear}"
                         decadeyear_confirm = True
-                        print(
-                            f'Selected: {i_decadeyear}\n')
                         # Linkler 1870 ve 2029'a kadar çalışıyor.
-                        if i_decadeyear > 1869 and i_decadeyear < 2030:
+                        if i_decadeyear >= 1870 and i_decadeyear <= 2029:
+                            print(f'Selected: {i_decadeyear}\n')
                             break
+                        else:
+                            print('Belirtilen aralıkta seçim yapınız.')
                 elif decadeyear_dory == "n":
                     w_decadeyear = ''
                     msg_decadeyear = ''
@@ -74,7 +84,7 @@ def filtre_sor():
                     # Gelen filtre adresini düzenliyoruz. Son kısmını alıyoruz
                     strip_genre = genre_filter_adress.replace(
                         f'/{user_name}/list/{list_name}/detail', "")
-                    msg_genre = f'Genre:   [{filter_num}]: {genre_filter_name}\n'
+                    msg_genre = f'  \u25E6 Genre:   [{filter_num}]: {genre_filter_name}\n'
                     w_genre = strip_genre
                     genre_confirm = True
                 elif genre_dory == "n":
@@ -97,7 +107,7 @@ def filtre_sor():
                     # Gelen filtre adresini düzenliyoruz. Son kısmını alıyoruz
                     strip_sortby = sortby_filter_adress.replace(
                         f'/{user_name}/list/{list_name}/detail', "")
-                    msg_sortby = f'Sort By: [{filter_num}]: {sortby_filter_name}\n'
+                    msg_sortby = f'  \u25E6 Sort By: [{filter_num}]: {sortby_filter_name}\n'
                     w_sortby = strip_sortby
                     sortby_confirm = True
                 elif sortby_dory == "n":
@@ -170,14 +180,47 @@ def signature(x):
     # x: 0 ilk, 1 son
     try:
         if x == True:
+
             # İlk imza gösteriminde öncesini temizle
             os.system('cls')
+            print('Your Inputs;', end='')
             if w_filter and filter_empty_items < 3:
                 print(
-                    f'User: {user_name}\nList: {list_name}\nFiltre uygulaması: Var\n---- Filtreler;\n{filtres_msg}')
+                    f'\u00B7 User: {user_name}\n\u00B7 List: {list_name}\n\u00B7 Filtre uygulaması: Var\n{filtres_msg}')
             else:
                 print(
-                    f'\nUser: {user_name}\nList: {list_name}\nFiltre uygulaması: Yok\n')
+                    f'\n\u00B7 User: {user_name}\n\u00B7 List: {list_name}\n\u00B7 Filtre uygulaması: Yok\n')
+
+            # Liste sayfasından bilgiler çekmek.
+            try:
+                print('List info;')
+                # > Liste sahibin ismini çektik
+                search_listBy = soup.select("[itemprop=name]")[0].text
+                print(f'\u00B7 List by {search_listBy} (@{user_name})')
+                # > Liste başlığının ismini çektik
+                search_listTitle = soup.select("[itemprop=title]")[0].text
+                print(f'\u00B7 List title "{search_listTitle}" ')
+                # > Liste oluşturulma tarihi
+                search_listPtime = soup.select(".published time")[0].text
+                # > arrow: https://arrow.readthedocs.io/en/latest/
+                listPtime = arrow.get(search_listPtime)
+                print(f'\u00B7 Published: {listPtime.humanize()}')
+                # < or: print(f'Published: {listPtime.humanize(granularity=["year","month", "day", "hour", "minute"])}')
+                # > utcnow: https://newbedev.com/python-utc-datetime-object-s-iso-format-doesn-t-include-z-zulu-or-zero-offset
+                # now_time = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+                # s = arrow.get(now_time) | time_x = s-f | print(f'Time since publication: {time_x}')
+                # > Search list update time
+                try:
+                    search_listUtime = soup.select(".updated time")[0].text
+                    listUtime = arrow.get(search_listUtime)
+                    msg_Utime = listUtime.humanize()
+                except:
+                    msg_Utime = 'No editing'
+                finally:
+                    print(f'\u00B7 Updated: {msg_Utime}')
+            except:
+                logging(
+                    'Film sahibi görünür adı ve liste adı istenirken hata oluştu.')
         else:
             print(
                 f'\nFilename: {csv_name}\nFilm sayısı: {dongu_no-1}\nTüm filmler {csv_name + " dosyasına"} aktarıldı.')
@@ -216,21 +259,25 @@ def rst():
 
 
 def dir_check(l, e):
+    # Buradaki ifler tekli kontrol yapmamıza yarar
+    # < Örneğin e'yi false yollarım ve sadece l'yi çekebilirim.
     # l= logdir_name, e = exdir_name
-    # Log Dir
-    if os.path.exists(l):
-        logging(f'{l} klasörü halihazırda var.')
-    else:
-        os.makedirs(l)
-        logging(f'{l} klasörü oluşturuldu')
-        # Oluşturulmaz ise bir izin hatası olabilir
-    # Exports Dir
-    if os.path.exists(e):
-        logging(f'{e} klasörü halihazırda var.')
-    else:
-        os.makedirs(e)
-        logging(f'{e} klasörü oluşturuldu')
-        # Oluşturulmaz ise bir izin hatası olabilir
+    if l:
+        # Log Dir
+        if os.path.exists(l):
+            logging(f'{l} klasörü halihazırda var.')
+        else:
+            os.makedirs(l)
+            logging(f'{l} klasörü oluşturuldu')
+            # Oluşturulmaz ise bir izin hatası olabilir
+    if e:
+        # Exports Dir
+        if os.path.exists(e):
+            logging(f'{e} klasörü halihazırda var.')
+        else:
+            os.makedirs(e)
+            logging(f'{e} klasörü oluşturuldu')
+            # Oluşturulmaz ise bir izin hatası olabilir
 
 
 def countrooms(r_article):
@@ -245,7 +292,7 @@ def countrooms(r_article):
 
 def logging(r_message):
     try:
-        f = open(f'{logdir_name}/{csv_name}.txt', "a")
+        f = open(open_log, "a")
         f.writelines(f'{r_message}\n')
         f.close()
     except:
@@ -263,7 +310,7 @@ def f_filmsayisi(r_lastPage_No):
         # < Film sayısı öğrenildi.
         # > Toplam film sayısını belirlemek.
         film_sayisi = ((int(r_lastPage_No)-1)*100)+lastpage_fcount
-        logging(f"{csv_name} Bilgi: Film sayısı {film_sayisi} olarak bulunmuştur.")
+        logging(f"Bilgi: Film sayısı {film_sayisi} olarak bulunmuştur.")
         return film_sayisi
     except:
         print('Film sayısını elde ederken hata.')
@@ -274,10 +321,10 @@ def readpage(r_url):
         page_url = requests.get(r_url)
         # > Sayfa kodları çekildi.
         soup = BeautifulSoup(page_url.content.decode('utf-8'), 'html.parser')
-        logging(f'{csv_name} Connect to: {r_url}')
+        logging(f'Trying connect to: {r_url}')
         return soup
     except:
-        print('Sayfa okunurken bir hata oluştu.')
+        print('Connection to address failed.')
 
 
 def pullfilms(r_count, r_soup):
@@ -306,32 +353,63 @@ def pullfilms(r_count, r_soup):
         print('Film bilgilerini elde ederken bir hatayla karşılaşıldı.')
 
 
-# > Kullanıcı eğer domainden değilde direkt olarak girerse yazıyı küçültüyoruz.
-user_name = str(input("> Username(Not display name): ")).lower()
-list_name = str(input("> Listname(Domain): ")).lower()
-# > Dosya çakışma sorunları için farklı isimler ürettik.
-csv_name = f"{user_name}-({zaman})"
-logdir_name = "logs"
-exdir_name = "exports"
-# Gerekli klasörlerin kontrolü
-dir_check(logdir_name, exdir_name)
+def check_user():
+    try:
+        user_displayname = visit_profile.select(".title-1")[0].text
+        print(f'Found it: {user_displayname}')
+        logging(f'{user_name} kullanıcısı bulundu: {user_displayname}')
+        user_available = True
+    except:
+        print('Kullanıcı bulunamadı. Tekrar deneyin.')
+        logging(f'{user_name} kullanıcısı bulunamadı.')
+        user_available = False
+    finally:
+        return user_available
+
+
 # > Saf domain'in parçalanarak birleştirilmesi
-url_protocol, site_url = "https://", "letterboxd.com"
+url_protocol, site_url = "https://", "letterboxd.com/"
 domain = url_protocol + site_url
+# Gerekli dizin ve dosya isimlendirmesi.
+exdir_name, logdir_name, = "exports", "logs"
+# Burada loh dosyasına oturum onaylanana kadar geçici bir isimlendirme atanır.
+open_log = f'{logdir_name}/{run_time}.txt'
+# Log klasörünün kontrolü
+dir_check(logdir_name, False)
+
+while True:
+    # > Kullanıcı eğer domainden değilde direkt olarak girerse yazıyı küçültüyoruz.
+    user_name = str(input("> Username(Not display name): ")).lower()
+    # Kullanıcı mevcutmu bakıyoruz
+    visit_profile = readpage(f'{domain}{user_name}')
+    user_available = check_user()
+    if user_available:
+        break
+# > Dosya çakışma sorunları için farklı isimler ürettik.
+csv_name = f"{user_name}-({run_time})"
+# Artık log ismi karışmaması için log dosyasının ismini değiştik.
+# Os rename ile dizin belirterek dosya taşınabilir
+approved_session = f'{logdir_name}/{csv_name}.txt'
+os.rename(open_log, approved_session)
+open_log = approved_session
+open_csv = f'{exdir_name}/{csv_name}.csv'
+list_name = str(input("> Listname(Domain): ")).lower()
 # Misafir girişi için url oluşturuluyor
-pure_url = f'{domain}/{user_name}/list/{list_name}/detail'
+pure_url = f'{domain}{user_name}/list/{list_name}/detail/'
 soup = readpage(pure_url)
-# Filtrelerin domaine uygulanması
+# Filtrelerin çekilmesi ve domaine uygulanması
 all_filtres, w_filter, filtres_msg, filter_empty_items = filtre_sor()
 url = f'{pure_url}{all_filtres}page/'
 logging(f'Filtreler: {all_filtres}\nFiltre Url\'ye işlendi: {url}')
-# > Domain'in doğru olup olmadığı kullanıcıya sorulur, doğruysa kullanıcı enter'a basar ve program verileri çeker.
+# Karşılama mesajı, kullanıcının girdiği bilgleri ve girilen bilgilere dayanarak listenin bilgilerini yazdırır.
 signature(1)
-logging(f'{csv_name} Bilgi: Karlışama imzası yazıldı')
+# > Domain'in doğru olup olmadığı kullanıcıya sorulur, doğruysa kullanıcı enter'a basar ve program verileri çeker.
 ent = input(
-    f"Link: {pure_url}{all_filtres}\n> Press enter to confirm the entered information. (Enter)\n").lower()
-
+    f"\u00B7 Link: {pure_url}{all_filtres}\n\n> Press enter to confirm the entered information. (Enter)\n").lower()
 if ent == "":
+    # Gerekli klasörlerin kontrolü
+    dir_check(False, exdir_name)
+
     # > Burada pure_url ile liste sayfa sayısını elde ediyoruz ve aşağısında film toplamı sayısını hesaplıyoruz
     logging('Saf sayfaya erişim başlatılıyor.')
     try:
@@ -353,19 +431,19 @@ if ent == "":
             f'Saf sayfa ile iletişim tamamlandı. Listedeki sayfa sayısının {lastPage_No} olduğu öğrenildi.')
 
     # Konumda klasör yoksa dosya oluşturmayacaktır.
-    with open(f'{exdir_name}/{csv_name}.csv', 'w', newline='', encoding="utf-8") as file:
+    with open(f'{open_csv}', 'w', newline='', encoding="utf-8") as file:
         writer = csv.writer(file)
         writer.writerow(["Sıra", "Filmİsmi", "YayınYılı"])
         # Filmleri çekiyoruz
         dongu_no = 1
         # x sıfırdan başlıyor
         for x in range(int(lastPage_No)):
-            logging(f'{csv_name} Connecting to: {url}{str(x+1)}')
+            logging(f'Connecting to: {url}{str(x+1)}')
             current_soup = readpage(f'{url}{str(x+1)}')
             dongu_no = pullfilms(dongu_no, current_soup)
         # Açtığımız dosyayı manuel kapattık
         file.close()
-    logging(f'{csv_name} Success!')
+    logging(f'Success!')
     signature(0)
     rst()
 else:
