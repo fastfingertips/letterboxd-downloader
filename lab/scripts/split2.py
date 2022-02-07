4cd4b0ba478fb50d0503475701fb01f2d8e0b415
FastFingertips <bugra.coskun@outlook.com>
2022-02-07 11:35:31 +0300

    Deleted: Scripts

--- lab/scripts/split2.py
+++
@@ -1,9 +0,0 @@
-csvPath = input('CSV NAME: ')
-csvfile = open(csvPath, 'r', encoding="utf8").readlines() #: lines list
-filename = 1
-split = 1500
-for i in range(len(csvfile)):
-    if i % split == 0:
-        csvfile.insert(i+split,csvfile[0]) #: keep header
-        open(str(filename) + '.csv', 'w+', encoding="utf8").writelines(csvfile[i:i+split])
-        filename += 1
