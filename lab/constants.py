e26fd2ee0098358b0c6dd68ab5c333b25893cd3f
FastFingertips <bugra.coskun@outlook.com>
2023-02-10 20:20:27 +0300

    Create constants.py

---
+++ lab/constants.py
@@ -0,0 +1,14 @@
+# Domain
+SITE_PROTOCOL, SITE_URL = "https://", "letterboxd.com"
+SITE_DOMAIN = SITE_PROTOCOL + SITE_URL
+# Log Pre
+PRE_LOG_INFO = "Info: "
+PRE_LOG_ERR = "Error: "
+# Cmd Lines
+SUP_LINE = '_'*80
+SUB_LINE = '¯'*80
+SETTINGS_FILE_NAME = 'settings'+'.json'
+CMD_PRINT_FILMS = True
+SPLIT_LIMIT = 1500
+PARTITION_LIMIT = 10
+SPLIT_PARAMETER = "split:"
