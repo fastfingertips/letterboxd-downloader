bc66d144983cea41ca6684c0266dac8c5e52fe63
FastFingertips <bugra.coskun@outlook.com>
2023-07-14 11:42:09 +0300

    Update getListSignature() function syntax.
    Added English comments.

--- lab/methods/req_.py
+++ lab/methods/req_.py
@@ -89,33 +89,54 @@ def userListCheck(_url_list_item_dom, urlListItem): #: Kullanıcının girilen �
         currentListAvaliable = False
     finally: return currentListAvaliable, metaOgUrl
 
-def getListSignature(_listDict): # Get list's signature
+def getListSignature(_listDict): # get list's signature
+    #> extract the DOM element and detail page of the list
     listDom = _listDict['list_dom']
     listDetailPage = _listDict['list_detail_page']
 
-    listBy = listDom.select("[itemprop=name]")[0].text #: Liste sahibi ismi çekiliyor.
-    try: listTitle = listDom.select("[itemprop=title]")[0].text.strip() #: Liste başlığının ismini çekiliyor.
-    except IndexError: listTitle = getMetaContent(listDom, 'og:title') #: Liste başlığının ismini çekiliyor.
-    listPublicationTime = listDom.select(".published time")[0].text #: Liste oluşturulma tarihi çekiliyor.
+    #> get the name of the list owner.
+    listBy = listDom.select("[itemprop=name]")[0].text 
+
+    #> get the title of the list.
+    try:
+        listTitle = listDom.select("[itemprop=title]")[0].text.strip()
+    except IndexError:
+        #> get the title of the list from the meta tag if it's not found in the DOM.
+        listTitle = getMetaContent(listDom, 'og:title')
+
+    #> get the creation date of the list.
+    listPublicationTime = listDom.select(".published time")[0].text
+
+    #> convert the creation date to a human-readable format.
     listPublicationTimeHumanize = arrow.get(listPublicationTime).humanize()
-    listLastPage = getListLastPageNo(listDom, listDetailPage) #: Liste sayfa sayısı çekiliyor.
-    listMovieCount =  getMovieCount(listLastPage, listDom, listDetailPage) #: Listedeki film sayısı hesaplanıyor.
 
-    try: ## Filtre bilgilerini liste sayfasından edinmeyi denemek.
-        domSelectedDecadeYear = listDom.select(".smenu-subselected")[3].text + 'movies only was done by.' #: Liste sayfasından yıl aralık filtre bilgisi alınıyor.
-        domSelectedGenre = listDom.select(".smenu-subselected")[2].text + 'only movies.' #: Liste sayfasından tür filtre bilgisi alınıyor.
-        domSelectedSortBy = listDom.select(".smenu-subselected")[0].text + '.' #: Liste sayfasından sıralama filtre bilgisi alınıyor.
-    except Exception as e: ## Filtre bilgileri edinirken bir hata oluşursa..
-        txtLog(f'{PRE_LOG_ERR}Filtre bilgileri elde bir sorun gerçekleşti.')
-        print('Filtre bilgileri elde bir sorun gerçekleşti.')
-        domSelectedDecadeYear, domSelectedGenre, domSelectedSortBy = 3*'Unknown' #: Filtre bilgileri edinemediğinde her filtreye None eklenir.
+    #> get the number of pages in the list.
+    listLastPage = getListLastPageNo(listDom, listDetailPage)
 
-    try: ## Search list update time
-        listUpdateTime = listDom.select(".updated time")[0].text #: Liste düzenlenme vakti çekiliyor.
-        listUT = arrow.get(listUpdateTime).humanize() #: Çekilen liste düzenlenme vakti okunmaya uygun hale getiriliyor.
-    except Exception as e: #: Düzenleme vakti edinemezse.
+    #> calculate the number of movies in the list.
+    listMovieCount =  getMovieCount(listLastPage, listDom, listDetailPage)
+
+    try: ## try to extract filter information from the list page.
+        #> get the year range filter information from the list page.
+        domSelectedDecadeYear = listDom.select(".smenu-subselected")[3].text + 'movies only was done by.' 
+        #> get the genre filter information from the list page.
+        domSelectedGenre = listDom.select(".smenu-subselected")[2].text + 'only movies.'
+        #> get the sorting filter information from the list page.
+        domSelectedSortBy = listDom.select(".smenu-subselected")[0].text + '.'
+    except Exception as e: ## if there is an error while extracting filter information...
+        txtLog(f'{PRE_LOG_ERR}A problem occurred while retrieving filter information.')
+        print('A problem occurred while retrieving filter information.')
+        #> set each filter to 'Unknown' if the filter information cannot be obtained.
+        domSelectedDecadeYear, domSelectedGenre, domSelectedSortBy = 3*'Unknown'
+
+    try: ## get the update time of the list
+        listUpdateTime = listDom.select(".updated time")[0].text
+        #> convert the update time to a human-readable format.
+        listUT = arrow.get(listUpdateTime).humanize()
+    except Exception as e: ## if the update time cannot be obtained...
         errorLine(e)
-        listUT = 'No editing.' #: Hata alınırsa liste düzenlenmemiş varsayılır.
+        #> assume that the list has not been edited in case of an error.
+        listUT = 'No editing.'
 
     listSign = {
         'list_by': listBy,
