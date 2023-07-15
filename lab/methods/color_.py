7ade51717ed335a97ee0c50cf9e1b6bdebcef9e4
FastFingertips <bugra.coskun@outlook.com>
2023-07-15 20:02:17 +0300

    Update color_.py syntax.
    Edited functions and libraries.

--- lab/methods/color_.py
+++ lab/methods/color_.py
@@ -1,10 +1,14 @@
-from constants.project import SUP_LINE, CMD_PRINT_FILMS, SUB_LINE
-from termcolor import colored as ced #: [colored, cprint]
+from constants.project import(
+    CMD_PRINT_FILMS,
+    SUP_LINE,
+    SUB_LINE
+)
 
+from termcolor import colored as ced
 
-def cmdPre(m,c): #: Mesaj ön ekleri için kalıp.
-    if m[0] == " ": return f' {ced(m[1:],color=c)}  '
-    elif m[0] == "[": return f'[{ced(m[1:],color=c)}] '
+def cmdPre(_msg, _color):
+    if _msg[0] == " ": return f' {ced(_color[1:],color=_color)}  '
+    elif _msg[0] == "[": return f'[{ced(_color[1:],color=_color)}] '
 
 # INITIAL ASSIGNMENTS
 preBlankCount = 4*' ' # cmd msg pre blank calc
@@ -13,17 +17,17 @@ preCmdInfo = cmdPre('[#','yellow') # cmd info msg pre
 preCmdErr = cmdPre('[!','red') # cmd error msg pre
 preCmdCheck = cmdPre('[✓','green') # cmd check msg pre
 preCmdUnCheck = cmdPre('[X','red') # cmd uncheck msg pre
-preCmdMiddleDot, preCmdMiddleDotList  = cmdPre(u'[\u00B7','cyan'), cmdPre(u' \u00B7','cyan') #: Cmd middle dot pre
+preCmdMiddleDot, preCmdMiddleDotList  = cmdPre(u'[\u00B7','cyan'), cmdPre(u' \u00B7','cyan') #: cmd middle dot pre
 supLineFilms = f'{SUP_LINE}\n{preCmdInfo}{ced("Movies on the list;", color="yellow")}\n' if CMD_PRINT_FILMS else ''
 
-def cmdBlink(m,c):
-    return ced(m,c,attrs=["blink"])
+def cmdBlink(_msg, _color):
+    return ced(_msg, _color, attrs=["blink"])
 
-def coloredDictPrint(colored_dict, main_title=None): # title, dict
+def coloredDictPrint(_coloredDict, _mainTitle=None): # title, dict
     print(SUP_LINE)
-    if main_title != None: print(f"{preCmdInfo}{ced(f'{main_title}', color='yellow')}")
-    for listHeader in colored_dict:
+    if _mainTitle != None: print(f"{preCmdInfo}{ced(f'{_mainTitle}', color='yellow')}")
+    for listHeader in _coloredDict:
         print(f"{preCmdMiddleDot}{ced(listHeader, attrs=['bold', 'underline'])}")
-        for listHeaderItem in colored_dict[listHeader]:
-            print(f"{preCmdMiddleDotList}{listHeaderItem}: {ced(colored_dict[listHeader][listHeaderItem],'blue')}")
+        for listHeaderItem in _coloredDict[listHeader]:
+            print(f"{preCmdMiddleDotList}{listHeaderItem}: {ced(_coloredDict[listHeader][listHeaderItem],'blue')}")
     print(SUB_LINE)
