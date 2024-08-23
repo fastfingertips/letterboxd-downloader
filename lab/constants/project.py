9bce209817b347732085104c37803f2373c86b1c
FastFingertips <bugra.coskun@outlook.com>
2024-08-23 23:36:37 +0300

    refactor(constants): improve readability and organization of constants

--- lab/constants/project.py
+++ lab/constants/project.py
@@ -1,28 +1,33 @@
-SETTINGS_FILE_NAME = 'settings'+'.json' # settings file name
-SPLIT_LIMIT = 1500 # split limit
-PARTITION_LIMIT = 10 # partition limit
+# File Names
+SETTINGS_FILE_NAME = 'settings.json'
+SESSIONS_FILE_NAME = 'sessions.json'
+
+# Limits
+SPLIT_LIMIT = 1500
+PARTITION_LIMIT = 10
+
 # Domain
-SITE_PROTOCOL, SITE_URL = "https://", "letterboxd.com"
-SITE_DOMAIN = SITE_PROTOCOL + SITE_URL # https://letterboxd.com
-# Log
-DEFAULT_LOG_KEY = 'log_dir' # default log dir name
-DEFAULT_EXPORT_KEY = 'export_dir' # default export dir name
-PRE_LOG_INFO = "Info: " # log file ingo msg pre
-PRE_LOG_ERR = "Error: " # log file err msg pre  
-# Cmd Lines
-SUP_LINE = '_'*80 # sup line lenght
-SUB_LINE = '¯'*80 # sub line lenght
-CMD_PRINT_FILMS = True # Print the movies after every request?
-SPLIT_PARAMETER = "split:" # split parameter
-# Session
-SESSION_DICT_KEY = 'sessions' # sessions key
-SESSION_PROCESSES_KEY = 'processes' # session processes key
-SESSION_PIDS_KEY = 'pids' # session pids key
+SITE_PROTOCOL = "https://"
+SITE_URL = "letterboxd.com"
+SITE_DOMAIN = SITE_PROTOCOL + SITE_URL
 
-SESSIONS_FILE_NAME = 'sessions'+'.json' # sessions file name
+# Logging
+DEFAULT_LOG_KEY = 'log_dir'  # Default log directory name
+DEFAULT_EXPORT_KEY = 'export_dir'  # Default export directory name
+PRE_LOG_INFO = "Info: "  # Prefix for log information messages
+PRE_LOG_ERR = "Error: "  # Prefix for log error messages
 
-SESSION_PID_KEY = 'pid' # session pid key
-SESSION_START_KEY = 'start_key' # session start key
-SESSION_LAST_KEY = 'last_key' # session end key
+# Command Lines
+SUP_LINE = '_' * 80  # Upper line length
+SUB_LINE = '¯' * 80  # Lower line length
+CMD_PRINT_FILMS = True  # Print the movies after every request?
+SPLIT_PARAMETER = "split:"
 
-SESSION_FINISHED_KEY = 'finished' # session finished key
+# Session Keys
+SESSION_DICT_KEY = 'sessions'
+SESSION_PROCESSES_KEY = 'processes' 
+SESSION_PIDS_KEY = 'pids' 
+SESSION_PID_KEY = 'pid'
+SESSION_START_KEY = 'start_key'
+SESSION_LAST_KEY = 'last_key'
+SESSION_FINISHED_KEY = 'finished'
