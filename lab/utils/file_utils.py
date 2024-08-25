263782904270f3e75b78505497bfde93d6a0f846
FastFingertips <bugra.coskun@outlook.com>
2024-08-26 02:52:14 +0300

    refactor(utils): rename functions and improve docstrings in file_utils.py
    - Renamed functions to be more descriptive.
    - Enhanced docstrings for better clarity and consistency.

--- lab/utils/file_utils.py
+++ lab/utils/file_utils.py
@@ -3,21 +3,27 @@ from constants.terminal import PRE_CMD_INFO
 from utils.cmd_format import cmd_blink
 from utils.time.custom import get_run_time
 
-def dirCheck(dirs: list) -> None:
+def ensure_directories_exist(dirs: list) -> None:
     """
-    Check if a directory exists, if not create it
-    """
-    for dir in dirs:
-        if dir:
-            if os.path.exists(dir):
-                print(f'{PRE_CMD_INFO}Directory checked: {cmd_blink(dir, "yellow")}')
-            else:
-                os.makedirs(dir)
-                print(f'{PRE_CMD_INFO}Directory created: {cmd_blink(dir, "yellow")}')
+    Checks if the specified directories exist. Creates them if they do not exist.
 
-def fileCheck(files: list) -> None:
+    Args:
+        dirs (list): List of directory paths to check or create.
     """
-    Check if a file exists, if not create it
+    for directory in dirs:
+        if directory:
+            if os.path.exists(directory):
+                print(f'{PRE_CMD_INFO}Directory checked: {cmd_blink(directory, "yellow")}')
+            else:
+                os.makedirs(directory)
+                print(f'{PRE_CMD_INFO}Directory created: {cmd_blink(directory, "yellow")}')
+
+def ensure_files_exist(files: list) -> None:
+    """
+    Checks if the specified files exist. Creates them if they do not exist.
+
+    Args:
+        files (list): List of file paths to check or create.
     """
     for file in files:
         if file:
@@ -26,38 +32,67 @@ def fileCheck(files: list) -> None:
             else:
                 with open(file, 'w') as f:
                     if file.endswith('.json'): 
-                        print(f'{PRE_CMD_INFO}json file created: {cmd_blink(file, "yellow")}')
+                        print(f'{PRE_CMD_INFO}JSON file created: {cmd_blink(file, "yellow")}')
                         f.write('{}')
                     elif file.endswith('.csv'): 
-                        print(f'{PRE_CMD_INFO}csv file created: {cmd_blink(file, "yellow")}')
-                    else: f.write('')
+                        print(f'{PRE_CMD_INFO}CSV file created: {cmd_blink(file, "yellow")}')
+                    else:
+                        f.write('')
 
-def fileRenamer(old_name, new_name) -> bool:
+def rename_file_with_timestamp(old_name: str, new_name: str) -> bool:
     """
-    Rename a file
+    Renames a file by appending the current timestamp to the new name.
+
+    Args:
+        old_name (str): The current name of the file.
+        new_name (str): The new name for the file.
+
+    Returns:
+        bool: True if the file was renamed successfully, False if a file with the new name already exists.
     """
     current_time = get_run_time()
     new_name = f'{new_name}'.replace('.', f'{current_time}.')
-    if os.path.exists(new_name): return False
+    if os.path.exists(new_name):
+        return False
     os.rename(old_name, new_name)
     return True
 
-def fileExists(_filePath) -> bool:
+def file_exists(file_path: str) -> bool:
     """
     Checks if a file exists at the given path.
-    """
-    return os.path.exists(_filePath)
 
-def checkFilename(_fileName, _removestring="\"|%:/,.\\[]<>*?") -> bool:
-    """
-    Checks if a filename is valid by ensuring it doesn't contain invalid characters.
-    """
-    return all(char not in _removestring for char in _fileName)
+    Args:
+        file_path (str): The path to the file.
 
-def cleanFilename(_sourcestring, _removestring="\"|%:/,.\\[]<>*?") -> str:
+    Returns:
+        bool: True if the file exists, False otherwise.
     """
-    Cleans a filename by removing invalid characters.
+    return os.path.exists(file_path)
+
+def is_valid_filename(filename: str, invalid_chars: str = "\"|%:/,.\\[]<>*?") -> bool:
     """
-    for currentChar in _removestring:
-        _sourcestring = _sourcestring.replace(currentChar, "")
-    return _sourcestring
+    Checks if a filename is valid by ensuring it does not contain invalid characters.
+
+    Args:
+        filename (str): The name of the file to check.
+        invalid_chars (str): A string containing invalid characters.
+
+    Returns:
+        bool: True if the filename does not contain invalid characters, False otherwise.
+    """
+    return all(char not in invalid_chars for char in filename)
+
+def clean_filename(filename: str, invalid_chars: str = "\"|%:/,.\\[]<>*?") -> str:
+    """
+    Removes invalid characters from a filename.
+
+    Args:
+        filename (str): The name of the file to clean.
+        invalid_chars (str): A string containing invalid characters to remove.
+
+    Returns:
+        str: The cleaned filename.
+    """
+    for char in invalid_chars:
+        filename = filename.replace(char, "")
+    return filename
