8f76988e5850d99282ec796c390fbb7de1b5c8b9
FastFingertips <bugra.coskun@outlook.com>
2024-08-26 23:20:02 +0300

    refactor terminal constants to use more descriptive icon names and add new icons

--- lab/constants/terminal.py
+++ lab/constants/terminal.py
@@ -1,20 +1,31 @@
-from utils.color.custom import format_command_message, colored_text
+from utils.color.custom import colored, format_command_message
 from constants.project import CMD_PRINT_FILMS, SUP_LINE
 
-# --- CONSTANTS ---
+ICON_INFO = format_command_message('[#', 'yellow')
+ICON_ERROR = format_command_message('[!', 'red')
+ICON_CHECK = format_command_message('[✓', 'green')
+ICON_INPUT = format_command_message('[>', 'green')
+ICON_UNCHECK = format_command_message('[X', 'red')
+ICON_QUESTION = format_command_message('[?', 'yellow')
 
-# Prefixes for different command messages
+ICON_ARROW_RIGHT = colored('->', 'yellow')
+ICON_ARROW_LEFT = colored('<-', 'yellow')
+
+ICON_CIRCLE = colored('○', 'yellow')
+ICON_CIRCLE_FULL = colored('●', 'yellow')
+
+ICON_SQUARE = colored('□', 'yellow')
+ICON_SQUARE_FULL = colored('■', 'yellow')
+
+ICON_DIAMOND = colored('◇', 'yellow')
+ICON_DIAMOND_FULL = colored('◆', 'yellow')
+
+ICON_MIDDLE_DOT = format_command_message(u'[\u00B7', 'cyan')
+ICON_MIDDLE_DOT_LIST = format_command_message(u' \u00B7', 'cyan')
+
 PRE_BLANK_COUNT = 4 * ' '  # Command message prefix blank calculation
-PRE_CMD_INPUT = format_command_message('[>', 'green')  # Command input message prefix
-PRE_CMD_INFO = format_command_message('[#', 'yellow')  # Command info message prefix
-PRE_CMD_ERR = format_command_message('[!', 'red')  # Command error message prefix
-PRE_CMD_CHECK = format_command_message('[✓', 'green')  # Command check message prefix
-PRE_CMD_UNCHECK = format_command_message('[X', 'red')  # Command uncheck message prefix
-PRE_CMD_MIDDLE_DOT = format_command_message(u'[\u00B7', 'cyan')  # Middle dot prefix for lists
-PRE_CMD_MIDDLE_DOT_LIST = format_command_message(u' \u00B7', 'cyan')  # Middle dot list item prefix
 
-# Conditional assignment for movies on the list
 SUP_LINE_FILMS = (
-    f'{SUP_LINE}\n{PRE_CMD_INFO}{colored_text("Movies on the list;", color="yellow")}\n'
+    f'{SUP_LINE}\n{ICON_INFO}{colored("Movies on the list;", color="yellow")}\n'
     if CMD_PRINT_FILMS else ''
 )
