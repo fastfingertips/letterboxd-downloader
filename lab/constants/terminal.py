94312683867e0c27d814d08b18203bf18fbcae6b
FastFingertips <bugra.coskun@outlook.com>
2024-08-26 19:12:54 +0300

    refactor: replace `cmd_pre` with `format_command_message`

--- lab/constants/terminal.py
+++ lab/constants/terminal.py
@@ -1,18 +1,18 @@
 from termcolor import colored as ced
-from utils.color.custom import cmd_pre
+from utils.color.custom import format_command_message
 from constants.project import CMD_PRINT_FILMS, SUP_LINE
 
 # --- CONSTANTS ---
 
 # Prefixes for different command messages
 PRE_BLANK_COUNT = 4 * ' '  # Command message prefix blank calculation
-PRE_CMD_INPUT = cmd_pre('[>', 'green')  # Command input message prefix
-PRE_CMD_INFO = cmd_pre('[#', 'yellow')  # Command info message prefix
-PRE_CMD_ERR = cmd_pre('[!', 'red')  # Command error message prefix
-PRE_CMD_CHECK = cmd_pre('[✓', 'green')  # Command check message prefix
-PRE_CMD_UNCHECK = cmd_pre('[X', 'red')  # Command uncheck message prefix
-PRE_CMD_MIDDLE_DOT = cmd_pre(u'[\u00B7', 'cyan')  # Middle dot prefix for lists
-PRE_CMD_MIDDLE_DOT_LIST = cmd_pre(u' \u00B7', 'cyan')  # Middle dot list item prefix
+PRE_CMD_INPUT = format_command_message('[>', 'green')  # Command input message prefix
+PRE_CMD_INFO = format_command_message('[#', 'yellow')  # Command info message prefix
+PRE_CMD_ERR = format_command_message('[!', 'red')  # Command error message prefix
+PRE_CMD_CHECK = format_command_message('[✓', 'green')  # Command check message prefix
+PRE_CMD_UNCHECK = format_command_message('[X', 'red')  # Command uncheck message prefix
+PRE_CMD_MIDDLE_DOT = format_command_message(u'[\u00B7', 'cyan')  # Middle dot prefix for lists
+PRE_CMD_MIDDLE_DOT_LIST = format_command_message(u' \u00B7', 'cyan')  # Middle dot list item prefix
 
 # Conditional assignment for movies on the list
 SUP_LINE_FILMS = (
