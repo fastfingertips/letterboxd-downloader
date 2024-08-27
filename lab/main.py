7f6bfe424bb085f65566f46c8b957546d23d25d5
FastFingertips <bugra.coskun@outlook.com>
2024-08-28 00:00:58 +0300

    refactor variable names for clarity and consistency in main.py

--- lab/main.py
+++ lab/main.py
@@ -50,16 +50,16 @@ def export_films_to_csv(csv_path: str, last_page_no: int, detail_page_url: str) 
         header = ['Year', 'Title', 'LetterboxdURI']
         writer.writerow(header) # csv açıldıktan sonra en üste yazılacak başlıklar.
 
-        loop_count = 1
+        count = 1
         print(SUP_LINE_FILMS, end='')
         for x in range(int(last_page_no)): # sayfa sayısı kadar döngü oluştur.
             txtLog(f'{PRE_LOG_INFO}Connecting to {detail_page_url}{str(x+1)}') # sayfa numarasını log dosyasına yaz.
             current_dom = fetch_page_dom(f'{detail_page_url}{str(x+1)}') # sayfa dom'u alınır.
-            loop_count = extract_and_write_films(loop_count, current_dom, writer) # filmleri al.
+            count = extract_and_write_films(count, current_dom, writer) # filmleri al.
         file.close() # açtığımız dosyayı manuel kapattık
 
     context = {
-        'count': loop_count
+        'count': count
     }
 
     return context
@@ -108,20 +108,23 @@ while True:
 
     print_session_hash(session_start_hash, session_current_hash)
 
-    #> while initializing
+    # While initializing
     listEnterPassOn = True
-    breakLoop = False
+    break_loop = False
     inputLoopNo = 0
     url_list = []
     while True:
-        inputLoopNo += 1 #: Başlangıçta döngü değerini artırıyoruz.
-        url_list_item = str(input(f'{ICON_INPUT}List URL[{inputLoopNo}]: ')).lower() #: Kullanıcıdan liste url'i alınması ve düzenlenmesi.
+        # Başlangıçta döngü değerini artırıyoruz.
+        inputLoopNo += 1 
+        # Kullanıcıdan liste url'i alınması ve düzenlenmesi.
+        url_list_item = str(input(f'{ICON_INPUT}List URL[{inputLoopNo}]: ')).lower()
         if len(url_list_item) > 0: ## Giriş boş değilse..
 
-            #> input starts with split parameter
+            # Input starts with split parameter
             if url_list_item[0:len(SPLIT_PARAMETER)] == SPLIT_PARAMETER: 
                 splitCsv(url_list_item[len(SPLIT_PARAMETER):], export_directory_name, session_current_hash)
-                inputLoopNo -= 1 #: Başarısız girişlerde döngü sayısının normale çevrilmesi.
+                # Restore loop number
+                inputLoopNo -= 1
                 continue
 
             if url_list_item[-1] == '.' and url_list_item[0] != '?':
@@ -130,18 +133,23 @@ while True:
                     inputLoopNo -= 1
                     continue
 
-            if '.' in url_list_item: # Girişte nokta varsa..
-                if url_list_item[-1] == '.' or url_list_item == '.': # Giriş nokta ile bitiyor veya tek nokta ise..
-                    breakLoop = True #: Url alımını sonlandıracak bilgi.
+            if '.' in url_list_item:
+                # Girişte nokta varsa..
+                if url_list_item[-1] == '.' or url_list_item == '.':
+                    # Giriş nokta ile bitiyor veya tek nokta ise..
+                    # Url alımını sonlandıracak bilgi.
+                    break_loop = True 
 
                     if not url_list_item[0] == '?':
                         print(f'{ICON_INFO}Parametre tanındı, liste alım işlemi sonlandırıldı.')
 
                     if url_list_item[0] == '?' or url_list_item == ".." or url_list_item[-2:] == "..": 
                         print(f'{ICON_INFO}Ek parametre tanındı, liste arama sonrası tüm listeler otomatik onaylanacak.')
-                        listEnterPassOn, listEnter, autoEnterMsg = False , True, '[Auto]' # Otomatik onaylama yapacak bilgi.
+                        # Otomatik onaylama yapacak bilgi.
+                        listEnterPassOn, listEnter, autoEnterMsg = False , True, '[Auto]'
 
-                    if len(url_list) > 0: #: URL listesi boş değilse
+                    if len(url_list) > 0:
+                        # URL listesi boş değilse
                         print(f'{ICON_INFO}Liste arama işlemi sonlandırıldı, Toplam {len(url_list)} liste girişi yapıldı.')
                         break
                     else:
@@ -153,29 +161,33 @@ while True:
                                 break
                             except:
                                 print(f"{ICON_INFO}To finish, you must first specify a list.") 
-                                inputLoopNo -= 1 #: Başarısız girişlerde döngü sayısının normale çevrilmesi.
+                                # Başarısız girişlerde döngü sayısının normale çevrilmesi.
+                                inputLoopNo -= 1 
                                 continue
-
-                url_list_item = trim_end(url_list_item, '.') #: Liste url'i parçalama.
+                url_list_item = trim_end(url_list_item, '.')
 
             if url_list_item[0] == '?': 
                 print(f'{ICON_INFO}Paremetre tanındı: Liste arama modu.')
-                url_list_item = url_list_item[1:] #: Başlangıçdaki soru işaret kaldırıldı.
+                # Başlangıçdaki soru işaret kaldırıldı.
+                url_list_item = url_list_item[1:]
 
-                if "!" in url_list_item: #: Son liste belirleyicisi
+                if "!" in url_list_item:
+                    # Son liste belirleyicisi
                     x = -1
-                    for i in range(3): #: Sona en fazla 3 rakam girilebilir. letterboxd'da max bulubilen liste sayısı 250
+                    for i in range(3):
+                        # Sona en fazla 3 rakam girilebilir. letterboxd'da max bulubilen liste sayısı 250
                         if url_list_item[x-1] == "!":
                             endList = int(url_list_item[x:])
                             url_list_item = url_list_item[:x-1]
                         x += -1
                 else:
-                    endList = 'Not specified.' # Son liste için bir parametre belirtilmezse.
+                    # Son liste için bir parametre belirtilmezse.
+                    endList = 'Not specified.'
 
                 searchList = f'{SITE_DOMAIN}/search/lists/{url_list_item}/'
                 searchListPreviewDom = fetch_page_dom(searchList)
-                searchMetaTitle = get_meta_content(searchListPreviewDom,'og:title') # getting og:title
-                searchLMetaUrl = get_meta_content(searchListPreviewDom,'og:url') #: Getting og:url
+                searchMetaTitle = get_meta_content(searchListPreviewDom,'og:title')
+                searchLMetaUrl = get_meta_content(searchListPreviewDom,'og:url')
 
                 try:
                     #: Kaç liste bulunduğu hakkında bilgi veren mesaj.
@@ -207,6 +219,8 @@ while True:
 
                 print(f"{ICON_INFO}Starting list search..")
 
+                """ XXX """
+
                 sayfa, liste = 0, 0
                 for i in range(int(searchListsQLastsPage)):
                     sayfa += 1
@@ -224,6 +238,7 @@ while True:
                         }
                     })
 
+                    """ Start: Processing and Validating URL Lists """
                     for listsUrl in listsUrls:
                         if liste == endList:
                             print(f"{ICON_INFO}Liste sayısı belirlenen sayıya ({colored(liste,'blue')}) ulaştı.")
@@ -239,49 +254,69 @@ while True:
                         else:
                             print(f'{ICON_UNCHECK}Bu listeyi daha önce eklemişiz.')
                             liste -= 1
-                    else: continue
+                    else:
+                        continue
+                    """ End: Processing and Validating URL Lists """
                     print(SUB_LINE)
                     break
+                """ XXX """
                 break
-            
-            # if url is detail page, remove detail part.
+            # If url is detail page, remove detail part.
             url_list_item = remove_substring(url_list_item, '/detail') 
-
-            urlListItemDom = fetch_page_dom(url_list_item) # sayfa dom'u alınır.
-            userListAvailable, approvedListUrl = check_user_list(urlListItemDom, url_list_item) # liste kullanılabilirliği ve Doğrulanmış URL adresi elde edilir.
-            if userListAvailable: # liste kullanıma uygunsa..
-                if approvedListUrl not in url_list: #> doğrulanmış URL daha önce URL Listesine eklenmediyse..
-                    url_list.append(approvedListUrl) # doğrulanmış URL, işlem görecek URL Listesine ekleniyor.
-                    if breakLoop: break # kullanıcı URL sonunda nokta belirttiyse.. URL alımını sonlandırıyoruz.
-                else: #> doğrulanmış URL daha önce işlem görecek URL listine eklenmiş ise..
-                    print(f'{ICON_ERROR}You have already entered this address list.') #: URL'in daha önce girildiğini ekrana yazdırıyoruz.
-                    inputLoopNo -= 1 # başarısız girişlerde döngü sayısının normale çevrilmesi.
-            else: #> kullanıcının girdiği URL doğrulanmazsa..
+            # Sayfa dom'u alınır.
+            urlListItemDom = fetch_page_dom(url_list_item)
+            # Liste kullanılabilirliği ve Doğrulanmış URL adresi elde edilir.
+            userListAvailable, approvedListUrl = check_user_list(urlListItemDom, url_list_item) 
+            if userListAvailable:
+                if approvedListUrl not in url_list:
+                    # Doğrulanmış URL daha önce URL Listesine eklenmediyse..
+                    # Doğrulanmış URL, işlem görecek URL Listesine ekleniyor.
+                    url_list.append(approvedListUrl)
+                    if break_loop:
+                        # Kullanıcı URL sonunda nokta belirttiyse.. URL alımını sonlandırıyoruz.
+                        break
+                else:
+                    # Doğrulanmış URL daha önce işlem görecek URL listine eklenmiş ise..
+                    # URL'in daha önce girildiğini ekrana yazdırıyoruz.
+                    print(f'{ICON_ERROR}You have already entered this address list.')
+                    # Başarısız girişlerde döngü sayısının normale çevrilmesi.
+                    inputLoopNo -= 1 
+            else:
+                # Kullanıcının girdiği URL doğrulanmazsa..
                 print(f"{ICON_INFO}You did not enter a valid url.")
-                inputLoopNo -= 1 # başarısız girişlerde döngü sayısının normale çevrilmesi.
-        else: #> kullanıcı genişliğe sahip bir değer girmez ise..
+                inputLoopNo -= 1
+        else:
+            # Kullanıcı genişliğe sahip bir değer girmez ise..
             print(f"{ICON_INFO}Just enter a period to move on to the next steps. You can also add it at the end of the URL.")
-            inputLoopNo -= 1 # başarısız girişlerde döngü sayısının normale çevrilmesi.
-    print(f"{ICON_INFO}List address acquisition terminated.") # liste url alımı sona erdğinde mesaj bastırılır.
+            inputLoopNo -= 1
 
-    processLoopNo = 0 # for döngüne ait 
+    print(f"{ICON_INFO}List address acquisition terminated.")
+
+    """ FOR INITIALIZING """
+
+    process_loop_no = 0
     for current_url in url_list:
-        processLoopNo += 1
-        process_state = f'[{processLoopNo}/{len(url_list)}]'
+        process_loop_no += 1
+        process_state = f'[{process_loop_no}/{len(url_list)}]'
         current_url_detail = f'{current_url}detail/' # url'e detail eklendi.
         current_url_detail_page = f'{current_url_detail}page/' # detaylı url'e sayfa gezintisi için parametre eklendi.
-        cListDom = fetch_page_dom(current_url_detail) # şu anki liste sayfasını oku.
+        current_dom_detail = fetch_page_dom(current_url_detail) # şu anki liste sayfasını oku.
 
+        # Liste sahibi bilgisini ayıklıyoruz.
         try:
-            current_list_owner = get_body_content(cListDom,'data-owner') # liste sahibini al.
+            current_list_owner = get_body_content(current_dom_detail, 'data-owner')
         except Exception as e:
             print(f'{ICON_ERROR}Liste sahibi bilgisi alınamadı')
             txtLog(f'Liste sahibi bilgisi alınamadı Hata: {e}')
             current_list_owner = 'Unknown'
 
-        current_list_domain = extract_list_domain_name(current_url) # liste domain ismini düzenleyerek alır.
-        current_list_runtime = get_run_time() # liste işlem vaktini al. 
+        # Liste domain ismini düzenleyerek alır.
+        # https://letterboxd.com/fastfingertips/list/list_name/ ~> list_name
+        current_list_domain = extract_list_domain_name(current_url)
+        # Liste işlem vaktini oluştur.
+        current_list_runtime = get_run_time()
 
+        # Liste hakkında bilgiler bastırılır.
         listSignature({
             "list_detail_page": current_url_detail_page, # https://letterboxd.com/{un}/list/{ln}/detail/page/
             "list_detail_url": current_url_detail, # https://letterboxd.com/{un}/list/{ln}/detail/
@@ -290,8 +325,8 @@ while True:
             "list_run_time": current_list_runtime, # 14072023022401
             "list_url": current_url, # https://letterboxd.com/{un}/list/{ln}
             "list_owner": current_list_owner, # {un}
-            "list_dom": cListDom # <html>...</html>
-        }) # liste hakkında bilgiler bastırılır.
+            "list_dom": current_dom_detail # <html>...</html>
+        })
 
         if listEnterPassOn:
             listEnter = input(f"{ICON_INPUT}Press enter to confirm the entered information. ({blink_text('Enter', 'green')})")
@@ -311,7 +346,7 @@ while True:
             print(SUP_LINE)
             print(f"{ICON_INFO}{colored(f'List confirmed. {autoEnterMsg}', color='green')}")
 
-            last_page_no = get_list_last_page_no(cListDom, current_url_detail_page)
+            last_page_no = get_list_last_page_no(current_dom_detail, current_url_detail_page)
             csv_path = ''.join([
                 exports_path,
                 current_list_owner, '_',
@@ -319,26 +354,27 @@ while True:
                 current_list_runtime, '.csv'
             ])
 
-            ensure_directories_exist([exports_path]) # export klasörünün kontrolü.
-            ensure_files_exist([csv_path]) # csv dosyasının kontrolü.
+            # Klasör ve dosya kontrolü.
+            ensure_directories_exist([exports_path])
+            ensure_files_exist([csv_path])
 
             loop_count = export_films_to_csv(csv_path, last_page_no, current_url_detail_page)
 
-            # process end
-            print(f'{ICON_INFO}{loop_count-1} film {blink_text(csv_path,"yellow")} dosyasına aktarıldı.') # print info
-            print(f"{ICON_INFO}{colored(f'{process_state} completed!', 'green')}") # print info
-            set_terminal_title(f'{process_state} completed!') # change title
-            txtLog(f'{PRE_LOG_INFO}{process_state} completed!') # log info
+            """ PROCESS END """
+
+            print(f'{ICON_INFO}{loop_count-1} film {blink_text(csv_path,"yellow")} dosyasına aktarıldı.')
+            print(f"{ICON_INFO}{colored(f'{process_state} completed!', 'green')}")
+            set_terminal_title(f'{process_state} completed!')
+            txtLog(f'{PRE_LOG_INFO}{process_state} completed!')
             print(SUB_LINE)
 
-    combineCsv(url_list, export_directory_name, session_current_hash, exports_path) # merge csv files
-
-    # session end
+    """ FOR END """
+    
+    # Merge csv files.
+    combineCsv(url_list, export_directory_name, session_current_hash, exports_path)
     endSession(session_current_hash)
-    set_terminal_title(f'Session: {session_current_hash} ended!') # change title
-    print(f"{ICON_INFO}{colored(f'Session: {session_current_hash} ended.', 'green')}") # print info
-    txtLog(f'{PRE_LOG_INFO}Session: {session_current_hash} ended.') # log info
-
-    # process end
+    set_terminal_title(f'Session: {session_current_hash} ended!')
+    txtLog(f'{PRE_LOG_INFO}Session: {session_current_hash} ended.')
+    print(f"{ICON_INFO}{colored(f'Session: {session_current_hash} ended.', 'green')}")
     print(f"{ICON_INFO}Process State: {blink_text(process_state +' Finish.','green')}")
     execute_terminal_command(f"echo {ICON_INFO}{blink_text('Press enter to continue with the new session.','yellow')} & pause >nul")
